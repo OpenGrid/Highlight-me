4ea96f90e0ba8b88dbf52be7250eb9dad17e72b6
Lech Wilczynski <lech.wilczynski@gmail.com>
2011-08-03 22:08:41 +0200

    no clue why post request response is empty

--- highlightme.js
+++ highlightme.js
@@ -1,8 +1,20 @@
 (function( $ ){
 
-  $.fn.highlightme = function() {
+  $.fn.highlightme = function(options) {
   
-    
-
+    return this.each(function() {
+        var t = $(this);
+        var data = {
+            code:t.text(), 
+            lexer:'js', 
+            style:"colorful", 
+            linenos:1, 
+            divstyles:"color:black;background:white;border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em .6em;"
+        };
+        $.post("http://www.hilite.me/api", data, function(r, status){
+            t.html(r);
+            console.log("Status: " + status);
+        }, 'html');
+    });
   };
 })( jQuery );
